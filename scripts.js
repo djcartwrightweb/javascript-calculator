e27f33bb043430722f4ac4bfca4a29878210630c
djcartwrightweb <dj.cartwright.web@gmail.com>
2022-08-27 21:35:41 -0400

    complete more js and css design work

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('mousedown', () => {
+        button.classList.remove('button-up');
+        button.classList.add('button-down');
+    });
+    button.addEventListener('mouseup', () => {
+        button.classList.remove('button-down');
+        button.classList.add('button-up');
+    });
+});
+
 const about = document.getElementById('ab');
 about.addEventListener('click', () => {
     document.getElementById('screen1').textContent = 'Made by DJ Cartwright, TOP, 2022';
     document.getElementById('screen2').textContent = '<== JS Calc ==>';
-});
-
-const clear = document.getElementById('clear');
-clear.addEventListener('click', () => {
-    document.getElementById('screen1').textContent = '';
-    document.getElementById('screen2').textContent = '0';
 });
 
 
